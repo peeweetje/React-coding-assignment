1fb77f07a5a04b775b05d14b0d921132b24d7119
Petra Vos <petravos70@gmail.com>
2019-02-24 14:48:33 +0100

    feat: Add loader and styling. Renamed the hooks of the checkboxes

--- src/Home.js
+++ src/Home.js
@@ -6,15 +6,18 @@ const Home = () => {
     window.localStorage.getItem("checked")
   ) || [{}];
   const [data, setData] = useState([]);
-  const [values, setValues] = useState(initialCheckValues);
+  const [isChecked, setChecked] = useState(initialCheckValues);
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
+    setLoading(true);
     const result = await fetch(
       "https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles.json"
     );
     const json = await result.json();
 
     setData(json);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -22,63 +25,71 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    window.localStorage.setItem("checked", JSON.stringify(values));
-  }, [values]);
+    window.localStorage.setItem("checked", JSON.stringify(isChecked));
+  }, [isChecked]);
 
   const toggleCheckboxes = ({ target: { id, checked } }) => {
-    setValues({ ...values, [id]: checked });
+    setChecked({ ...isChecked, [id]: checked });
   };
 
-  return (
-    <div className="main">
-      <div className="productlist-home">
-        {data.length > 0 &&
-          data.map(photo => (
-            <div className="productslists__card" key={photo.id}>
-              <div className={values[photo.id] ? "overlay" : ""} />
-              <Link
-                className="productslist__link"
-                to={{ pathname: `/article/${photo.id}`, state: photo }}
-              >
-                {photo.photos && photo.photos.length > 0 && (
-                  <div
-                    className="productslist__image"
-                    style={{
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      backgroundImage: `url("${
-                        photo.photos[0].files.original
-                      }")`
-                    }}
-                  />
-                )}
-                <p className="productslist__title">{photo.title}</p>
-              </Link>
-              <div
-                style={{
-                  display: "flex",
-                  height: "100%",
-                  alignItems: "flex-end"
-                }}
-              />
-              <label htmlFor={photo.id} className="read container">
-                {values[photo.id] ? "READ" : "UNREAD"}
-
-                <input
-                  id={photo.id}
-                  name={photo.id}
-                  className="checkbox"
-                  type="checkbox"
-                  checked={values[photo.id] || false}
-                  onChange={toggleCheckboxes}
-                />
-                <span className="checkmark" />
-              </label>
-            </div>
-          ))}
+  if (loading) {
+    return (
+      <div className="loader">
+        <span className="loading-clock" />
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="main">
+        <div className="productlist-home">
+          {data.length > 0 &&
+            data.map(photo => (
+              <div className="productslists__card" key={photo.id}>
+                <div className={isChecked[photo.id] ? "overlay" : ""} />
+                <Link
+                  className="productslist__link"
+                  to={{ pathname: `/article/${photo.id}`, state: photo }}
+                >
+                  {photo.photos && photo.photos.length > 0 && (
+                    <div
+                      className="productslist__image"
+                      style={{
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundImage: `url("${
+                          photo.photos[0].files.original
+                        }")`
+                      }}
+                    />
+                  )}
+                  <p className="productslist__title">{photo.title}</p>
+                </Link>
+                <div
+                  style={{
+                    display: "flex",
+                    height: "100%",
+                    alignItems: "flex-end"
+                  }}
+                />
+                <label htmlFor={photo.id} className="read container">
+                  {isChecked[photo.id] ? "READ" : "UNREAD"}
+
+                  <input
+                    id={photo.id}
+                    name={photo.id}
+                    className="checkbox"
+                    type="checkbox"
+                    checked={isChecked[photo.id] || false}
+                    onChange={toggleCheckboxes}
+                  />
+                  <span className="checkmark" />
+                </label>
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Home;
