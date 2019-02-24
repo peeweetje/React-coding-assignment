1d67ab69920429933de32d9c75f4abdb509c74f4
Petra Vos <petravos70@gmail.com>
2019-02-24 14:27:56 +0100

    feat: Display article and add styling

--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = props => {
+  console.log(props);
+  return (
+    <div className="flex-wrapper">
+      <div className="article-card">
+        {props.location.state && (
+          <>
+            <p className="article-card__title">{props.location.state.title}</p>
+            <div
+              className="article-card__image"
+              alt={props.location.state.title}
+              style={{
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundImage: `url("${
+                  props.location.state.photos[0].files.original
+                }")`
+              }}
+            />
+            <p className="article-card__description">
+              {props.location.state.description}
+            </p>
+          </>
+        )}
+      </div>
+      <Link className="home-link" to="/">
+        Back to Home
+      </Link>
+    </div>
+  );
 };
 
 export default Article;
