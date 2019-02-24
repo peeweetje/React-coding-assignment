28e50573f556082d2b82d88cd2d3bcee0a35d15a
Petra Vos <petravos70@gmail.com>
2019-02-24 13:56:41 +0100

    feat: Add home page and router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import Home from "./Home";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
 };
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
