b46c6693fb039bbd6c10bb578b4b3f60fe4c0359
Petra Vos <petravos70@gmail.com>
2019-02-24 15:15:27 +0100

    Refactor: add errorhandling + test. Add comments

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "./Home";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Home />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("checks all checkboxes", () => {
+  const checkBox = document.createElement("input");
+  ReactDOM.render(<Home />, checkBox);
+  expect(checkBox.checked).toEqual(false);
 });
--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,43 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
+  // Initial values for checkboxes.If stored in localStorage, use these,
+  // otherwise initialize as empty {}.
   const initialCheckValues = JSON.parse(
     window.localStorage.getItem("checked")
   ) || [{}];
   const [data, setData] = useState([]);
   const [isChecked, setChecked] = useState(initialCheckValues);
   const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const fetchData = async () => {
     setLoading(true);
-    const result = await fetch(
-      "https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles.json"
-    );
-    const json = await result.json();
 
-    setData(json);
+    try {
+      const result = await fetch(
+        "https://s3-eu-west-1.amazonaws.com/olio-staging-images/developer/test-articles.json"
+      );
+      const json = await result.json();
+      if (result.status === 404) {
+        throw new Error("Status 404");
+      }
+      setData(json);
+    } catch (err) {
+      setIsError(true);
+    }
     setLoading(false);
   };
 
+  // Fetch data on page load.
+  // The empty[] makes sure the data will be fetch only once,
+  // and not on every re-render.
   useEffect(() => {
     fetchData();
   }, []);
 
+  // Store checkbox state in localStorage, to persist data.
+  // [isChecked] makes sure we only store data when isChecked state changes.
   useEffect(() => {
     window.localStorage.setItem("checked", JSON.stringify(isChecked));
   }, [isChecked]);
@@ -32,6 +47,9 @@ const Home = () => {
     setChecked({ ...isChecked, [id]: checked });
   };
 
+  if (isError) {
+    return <div>Something went wrong ...</div>;
+  }
   if (loading) {
     return (
       <div className="loader">
@@ -43,26 +61,26 @@ const Home = () => {
       <div className="main">
         <div className="productlist-home">
           {data.length > 0 &&
-            data.map(photo => (
-              <div className="productslists__card" key={photo.id}>
-                <div className={isChecked[photo.id] ? "overlay" : ""} />
+            data.map(product => (
+              <div className="productslists__card" key={product.id}>
+                <div className={isChecked[product.id] ? "overlay" : ""} />
                 <Link
                   className="productslist__link"
-                  to={{ pathname: `/article/${photo.id}`, state: photo }}
+                  to={{ pathname: `/article/${product.id}`, state: product }}
                 >
-                  {photo.photos && photo.photos.length > 0 && (
+                  {product.photos && product.photos.length > 0 && (
                     <div
                       className="productslist__image"
                       style={{
                         backgroundSize: "cover",
                         backgroundPosition: "center",
                         backgroundImage: `url("${
-                          photo.photos[0].files.original
+                          product.photos[0].files.original
                         }")`
                       }}
                     />
                   )}
-                  <p className="productslist__title">{photo.title}</p>
+                  <p className="productslist__title">{product.title}</p>
                 </Link>
                 <div
                   style={{
@@ -71,15 +89,15 @@ const Home = () => {
                     alignItems: "flex-end"
                   }}
                 />
-                <label htmlFor={photo.id} className="read container">
-                  {isChecked[photo.id] ? "READ" : "UNREAD"}
+                <label htmlFor={product.id} className="read container">
+                  {isChecked[product.id] ? "READ" : "UNREAD"}
 
                   <input
-                    id={photo.id}
-                    name={photo.id}
+                    id={product.id}
+                    name={product.id}
                     className="checkbox"
                     type="checkbox"
-                    checked={isChecked[photo.id] || false}
+                    checked={isChecked[product.id] || false}
                     onChange={toggleCheckboxes}
                   />
                   <span className="checkmark" />
